6f479da146b628c0333ce6b183273624637b0dbb
Lassi Kortela <lassi@lassi.io>
2020-02-27 16:23:02 +0200

    Show the specific Unicode letters used by each SRFI

--- srfi-count-unicode.scm
+++ srfi-count-unicode.scm
@@ -4,6 +4,7 @@
         (scheme char)
         (scheme write)
         (srfi 1)
+        (srfi 69)
         (srfi 95))
 
 (import (chibi html-parser))
@@ -11,23 +12,31 @@
 
 (define (disp . xs) (for-each display xs) (newline))
 
+(define (hash-table-increment! table key)
+  (hash-table-update!/default table key (lambda (x) (+ x 1)) 0))
+
 (define (count-non-ascii-characters string)
   (define (ascii-char? char) (< (char->integer char) #x80))
-  (let ((in (open-input-string string)))
+  (let ((in (open-input-string string)) (chars (make-hash-table)))
     (let loop ((count 0))
       (let ((char (read-char in)))
-        (cond ((eof-object? char) count)
-              ((ascii-char? char) (loop count))
-              (else (loop (+ count 1))))))))
+        (cond ((eof-object? char)
+               (values count (hash-table-keys chars)))
+              ((ascii-char? char)
+               (loop count))
+              (else
+               (hash-table-increment! chars char)
+               (loop (+ count 1))))))))
 
 (define (count-non-ascii-per-srfi)
-  (filter (lambda (x) (not (= 0 (cdr x))))
+  (filter (lambda (x) (not (= 0 (cadr x))))
           (sort (map (lambda (srfi)
-                       (cons (srfi-number srfi)
-                             (count-non-ascii-characters
-                              (srfi-html srfi))))
+                       (let-values (((count chars)
+                                     (count-non-ascii-characters
+                                      (srfi-html srfi))))
+                         (cons* (srfi-number srfi) count chars)))
                      srfi-alist)
-                (lambda (a b) (> (cdr a) (cdr b))))))
+                (lambda (a b) (> (cadr a) (cadr b))))))
 
 (define (main) (for-each disp (count-non-ascii-per-srfi)))
 
